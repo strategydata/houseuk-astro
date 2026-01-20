de003e3761b824ad46ea360aa7300c8a2b186279
kangqiwang <kang7.wang@gmail.com>
2026-01-20 14:00:08 +0000

    Refactor S3 client initialization to include AWS credentials and improve logging

--- extract/landRegistry/src/execute.py
+++ extract/landRegistry/src/execute.py
@@ -16,7 +16,13 @@ def stream_to_s3(url: str ="http://prod.publicdata.landregistry.gov.uk.s3-websit
         key (str): The name of the key to upload to.
 
     """
-    s3 = boto3.client('s3')
+    logging.info("Creating S3 client")
+    logging.info(f"Uploading data from {os.getenv("AWS_ACCOUNT_ID")} to Secret {os.getenv("AWS_SECRET_ACCESS_KEY")} Access Key Id {os.getenv("AWS_ACCESS_KEY_ID")}")
+    s3 = boto3.client('s3',
+                    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+                    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+                    aws_account_id =os.getenv("AWS_ACCOUNT_ID")
+    )
 
     with requests.get(url, stream=True) as response:
         response.raise_for_status()
